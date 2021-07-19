05fe5558bffd2757d7eeb73da30a788ed5323f54
natalianancupil <n.nancupil.v@gmail.com>
2021-07-19 16:45:49 -0400

    inicio login

---
+++ src/login.js
@@ -0,0 +1,16 @@
+
+
+/*let htmlCode = ''
+htmlCode += `<p>HOLA</p>`
+
+divRoot.innerHTML = htmlCode*/
+
+export const login = () => {
+  
+    //const login = document.createElement('div');
+   console.log('Hola natalia!');
+
+   const viewLogin = `<p>HOLA</p>`;
+   return viewLogin;
+  };
+  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
-
+import { login } from './login.js';
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+document.getElementById("root").innerHTML= login();
