1ac84cc675b79fec513b79b540492105d9366d95
natalianancupil <n.nancupil.v@gmail.com>
2021-07-20 14:37:23 -0400

    vista login

--- src/login.js
+++ src/login.js
@@ -8,9 +8,28 @@ divRoot.innerHTML = htmlCode*/
 export const login = () => {
   
     //const login = document.createElement('div');
-   console.log('Hola natalia!');
+   console.log('Holi');
 
-   const viewLogin = `<p>HOLA</p>`;
+   const viewLogin = `<div class="containerGrid">
+                        <div class="logo">
+                            <img src="logoBike.png" alt="logo de la aplicación">
+                        </div>
+                        <div class="aboutApp">
+                            <p class="about">Mas info sobre nuestra comunidad <b>aquí</b></p>
+                        </div>
+                        <div class="loginButtons">
+                            <div class="typeButton" id="typeButton">
+                                <button type="button" class="signInButton">Ya tengo una cuenta</button>
+                                <button type="button" class="signUpButton">Crear una cuenta</button>
+                            </div>
+                            <div class="socialNetworkButton">
+                                <p>Iniciar sesión con:</p>
+                                <span class="iconify" data-inline="false" data-icon="flat-color-icons:google" style="font-size: 35px;"></span>
+                                <span class="iconify" data-inline="false" data-icon="logos:facebook" style="font-size: 33px;"></span>
+                            </div>
+                        </div>
+                     </div>`;
    return viewLogin;
   };
+ 
   
