210dd9cfe505e53faf19653ec9bffee87ad3b65e
natalianancupil <n.nancupil.v@gmail.com>
2021-07-21 08:24:24 -0400

    login css

--- src/login.js
+++ src/login.js
@@ -10,7 +10,7 @@ export const login = () => {
     //const login = document.createElement('div');
    console.log('Holi');
 
-   const viewLogin = `<div class="containerGrid">
+   const viewLogin = `<div class="containerLogin">
                         <div class="logo">
                             <img src="logoBike.png" alt="logo de la aplicación">
                         </div>
@@ -24,8 +24,10 @@ export const login = () => {
                             </div>
                             <div class="socialNetworkButton">
                                 <p>Iniciar sesión con:</p>
+                                <div class="icons">
                                 <span class="iconify" data-inline="false" data-icon="flat-color-icons:google" style="font-size: 35px;"></span>
                                 <span class="iconify" data-inline="false" data-icon="logos:facebook" style="font-size: 33px;"></span>
+                                </div>
                             </div>
                         </div>
                      </div>`;
