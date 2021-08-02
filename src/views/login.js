2d8c83a5b5ca635950bcea4562cf074c4bd8baec
Pancha-M <fran.munozm93@gmail.com>
2021-08-02 11:55:31 -0400

    cambios

--- src/views/login.js
+++ src/views/login.js
@@ -5,7 +5,7 @@ export const login = () => {
 
   const viewLogin = `<div class="containerLogin">
                             <div class="logo">
-                                <img src="../img/logoBike.png" alt="logo de la aplicación">
+                                <img src="../../src/img/logoBike.png" alt="logo de la aplicación">
                             </div>
                         <div class="loginElements">
                             <div class="typeButton" id="typeButton">
