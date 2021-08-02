9af5101b4328f0d8a539c05faca72fd9e9115279
Pancha-M <fran.munozm93@gmail.com>
2021-08-02 10:54:11 -0400

    docs para page

---
+++ docs/views/signIn.js
@@ -0,0 +1,48 @@
+import firebaseLoginFunctions from '../firebaseLogin.js';
+
+export const signIn = () => {
+  const containerViewSignIn = document.createElement('div');
+
+  const viewSignIn = `<div class="containerLogin">
+                            <div class="logo">
+                                <img src="img/logoBike.png" alt="logo de la aplicación">
+                            </div>
+                          <div class="loginElements">
+                          <p class="signInTitle">Ingresa tu mail y contraseña</p>
+                            <div class="formSignIn">
+                                <input type="email" class="inputSignIn" id="inputEmail" placeholder="Email">
+                                <input type="password" class="inputSignIn" id="inputPass" placeholder="Contraseña">
+                                <p class=passwordRecover>Recuperar Contraseña</p>
+                                <button class="signInEmailPass" id="signInEmailPass">Ingresar</button>
+                            </div>
+                            <div class="socialNetworkButton">
+                                <p>Acceder con:</p>
+                                <div class="icons">
+                                    <button class="googlebtn"  id="googlebtn">
+                                    <span class="iconify" id="googleButton" data-inline="false" data-icon="flat-color-icons:google" style="font-size: 35px;">
+                                    </span></button>
+                                </div>
+                                <footer="aboutContainer">
+                                  <p class="about">Mas info sobre nuestra comunidad <b>aquí</b></p>
+                                </footer>                   
+                              </div>
+                          </div>
+                       </div>`;
+  containerViewSignIn.innerHTML = viewSignIn;
+
+  // Iniciar sesion con email y constrana ya registrada
+  const signInEmailPass = containerViewSignIn.querySelector('#signInEmailPass');
+
+  signInEmailPass.addEventListener('click', () => {
+    const email = containerViewSignIn.querySelector('#inputEmail').value;
+    const password = containerViewSignIn.querySelector('#inputPass').value;
+    firebaseLoginFunctions.signInFunction(email, password);
+  });
+
+  const googleBtn = containerViewSignIn.querySelector('#googlebtn');
+  googleBtn.addEventListener('click', () => {
+    firebaseLoginFunctions.signInGoogle();
+  });
+
+  return containerViewSignIn;
+};
